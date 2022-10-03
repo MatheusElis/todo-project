bd4c4d2730cab11db0d4250eccf21915bcc2b41d
Matheus <matheuselis@id.uff.br>
2022-10-03 17:15:54 -0300

    add django todo app

---
+++ todo-django/todoapp/todolist/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods 
+
+
+from .models import Todo
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    return render(request, "base.html", {"todo_list": todos})
+
+@require_http_methods(["POST"])
+def add(request):
+    title = request.POST["title"]
+    todo = Todo(title = title)
+    todo.save()
+    return redirect("index")
+
+def update(request, todo_id):
+    todo = Todo.objects.get(id = todo_id)
+    todo.complete = not todo.complete 
+    todo.save()
+    return redirect("index")
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(id = todo_id)
+    todo.delete()
+    return redirect("index")
